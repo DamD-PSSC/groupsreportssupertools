9562534848e7a329550ec48128535e6f875fb033
SirNapz <spam@pssc.pl>
2020-05-15 23:01:36 +0200

    Prepared Footer component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './App.module.scss';
-import Logo from './components/atoms/Logo/Logo';
+import Footer from './components/organisms/Footer/Footer';
 
 function App() {
     return (
         <div>
             <h1 className="has-text-success">Hello world</h1>
-            <Logo />
+            <Footer />
         </div>
     );
 }
